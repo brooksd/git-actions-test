1cab3d0e981de13bce43a9bb9a5dd61c85004ddb
Donald Brooks Arum <brooksd079@gmail.com>
2022-06-13 23:45:11 +0300

    correct py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-
-def greeting():
-    print("Hello Moringa")
-
-
-def gitactions():
-    print("Welcome to Git Actions")
+def sum_numbers(numbers):
+    if len(numbers) == 0:
+        return 0
+    return numbers[0] + sum_numbers(numbers[1:])
 
 
-print(greeting())
+print(sum_numbers([1, 2, 3, 4, 5]))
