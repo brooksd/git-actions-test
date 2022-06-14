d0cee807adbbe1381357edff252e7a78aec2ea15
Donald Brooks Arum <brooksd079@gmail.com>
2022-06-14 08:52:02 +0300

    pylint meassage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def sum_numbers(numbers):
     if len(numbers) == 0:
         return 0
-    return numbers[0] + sum_numbers(numbers[1:])
+return numbers[0] + sum_numbers(numbers[1:])
 
 
 print(sum_numbers([1, 2, 3, 4, 5, 6, 7]))
